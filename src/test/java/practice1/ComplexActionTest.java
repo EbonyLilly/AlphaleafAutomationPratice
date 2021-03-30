4e558a71c173a6c3651aae00a6bdc6dd5ab1e470
Ebony Lilly <ebony.lilly25@gmail.com>
2021-03-29 21:09:14 -0400

    Added Branch commits

--- src/test/java/practice1/ComplexActionTest.java
+++ src/test/java/practice1/ComplexActionTest.java
@@ -8,4 +8,10 @@ public class ComplexActionTest {
         // Hello guys!!! I am on the main branch
         System.out.println("Hello");
     }
+    @Test
+    public void testCaseTwo(){
+        // Comment - you guys rock
+        System.out.println("Git Branch is actually easy");
+        System.out.println("Branch is separate version of project files & folders ");
+    }
 }
